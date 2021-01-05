7e9c6e7d2e9b9d5cfad0ec60c260bface0814ebc
bcshanken <bcshanken@gmail.com>
2021-01-05 15:10:33 -0500

    adding connection with atlas

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const path = require("path");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 const app = express();
